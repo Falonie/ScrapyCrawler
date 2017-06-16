fc49e29e827c46c83c3b0f0632266bd25e089996
Wangqingchen <541002901@qq.com>
2017-06-16 14:36:22 +0800

    ScrapyUpdate

--- amazon/amazon/amazon_sql.py
+++ amazon/amazon/amazon_sql.py
@@ -1,33 +1,35 @@
 import pymysql
-
-def amazon_sql():
-
-    connnection = pymysql.connect(host='localhost', user='root', password='1234', db='employee', charset='utf8mb4')
-    with connnection.cursor() as cursor:
-        sql = 'select * from scrapy_amazon'
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        for i, row in enumerate(results, 1):
-            print(i, row)
-
-if __name__ == '__main__':
-    amazon_sql()
-
-
-# class Amazon_sql():
 #
-#     def __init__(self):
-#         self.connect = pymysql.connect(host='localhost', user='root', password='1234', db='employee', charset='utf8mb4')
-#         self.cursor = self.connect.cursor()
+# def amazon_sql():
 #
-#     def process_item(self):
+#     connnection = pymysql.connect(host='localhost', user='root', password='1234', db='employee', charset='utf8mb4')
+#     with connnection.cursor() as cursor:
 #         sql = 'select * from scrapy_amazon'
-#         self.cursor.execute(sql)
-#         results = self.cursor.fetchall()
+#         cursor.execute(sql)
+#         results = cursor.fetchall()
 #         for i, row in enumerate(results, 1):
-#             # return i,row
 #             print(i, row)
 #
-# if __name__=='__main__':
-#     sql = Amazon_sql()
-#     sql.process_item()
+# if __name__ == '__main__':
+#     amazon_sql()
+
+
+class Amazon_sql():
+
+    def __init__(self):
+        self.connect = pymysql.connect(host='localhost', user='root', password='1234', db='employee', charset='utf8mb4')
+        self.cursor = self.connect.cursor()
+
+    def process_item(self):
+        sql = 'select * from scrapy_amazon'
+        self.cursor.execute(sql)
+        results = self.cursor.fetchall()
+        for i, row in enumerate(results, 1):
+            # item = (i, row)
+            # return item
+            print(i, row)
+
+if __name__=='__main__':
+    sql = Amazon_sql()
+    sql.process_item()
+    # print(sql.process_item())
--- ganji_truck_drivers/ganji_truck_drivers/pipelines.py
+++ ganji_truck_drivers/ganji_truck_drivers/pipelines.py
@@ -11,40 +11,45 @@ from .items import GanjiTruckDriversItem
 class GanjiTruckDriversPipeline(object):
 
     def __init__(self):
-    #     self.connection = pymysql.connect(host='localhost', user='root', password='1234', db='employee',
-    #                                       charset='utf8mb4')
-    #     self.cursor = self.connection.cursor()
 
-        self.book = xlwt.Workbook()
-        self.sheet = self.book.add_sheet('sheet', cell_overwrite_ok=True)
+        self.connection = pymysql.connect(host='localhost', user='root', password='1234', db='employee',
+                                          charset='utf8mb4')
+        self.cursor = self.connection.cursor()
 
+        # self.book = xlwt.Workbook()
+        # self.sheet = self.book.add_sheet('sheet', cell_overwrite_ok=True)
 
-    #
     def process_item(self, item, spider):
 
-    #     sql='insert into ganji_truck_drivers (TITLE,POSITION,RECRUIT,LOCATION,COMPANY,INDUSTRY,NATURE,SCALE' \
-    #         ',AUTHENTICATION,CREDIT_RANKING,JOB_DESCRIPTION,COMPANY_DESCRIPTION) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-    #     self.cursor.execute(sql, (
-    #     item['title'], item['position'], item['recruit'], item['location'], item['company'], item['industry'],
-    #     item['nature'], item['scale'], item['authentication'], item['credit_ranking'], item['job_description'],
-    #     item['company_description']))
-    #     self.connection.commit()
-    #
-        # with open('ganji3.csv', 'a+', newline='',encoding='utf-8') as f:
-        #     writer = csv.writer(f)
-        #     writer.writerow((item['title'], item['position'], item['recruit'], item['location'], item['company'],
-        #                      item['industry'], item['nature'], item['scale'], item['authentication'],
-        #                      item['credit_ranking'], item['job_description'], item['company_description']))
-    #
-    #     with open('ganji2.csv', 'a+', newline='') as f2:
-    #         writer = csv.writer(f2)
-    #         writer.writerow((item['title'], item['position'], item['recruit'], item['location'], item['company'],
-    #                          item['industry'], item['nature'], item['scale'], item['authentication'],
-    #                          item['credit_ranking']))
+        sql = 'insert into ganji_truck_drivers (TITLE,POSITION,RECRUIT,LOCATION,COMPANY,INDUSTRY,NATURE,SCALE,' \
+              'AUTHENTICATION,CREDIT_RANKING,JOB_DESCRIPTION,COMPANY_DESCRIPTION) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        self.cursor.execute(sql, (
+        item['title'], item['position'], item['recruit'], item['location'], item['company'], item['industry'],
+        item['nature'], item['scale'], item['authentication'], item['credit_ranking'], item['job_description'],
+        item['company_description']))
+        self.connection.commit()
 
-        self.sheet.write((item['title'], item['position'], item['recruit'], item['location'], item['company'],item['industry'], item['nature'],
-                          item['scale'], item['authentication'], item['credit_ranking'],item['job_description'], item['company_description']))
+        try:
+            with open('ganji3.csv', 'a+', newline='',encoding='utf-8') as f:
+                writer = csv.writer(f)
+                writer.writerow((item['title'], item['position'], item['recruit'], item['location'], item['company'],
+                                 item['industry'], item['nature'], item['scale'], item['authentication'],
+                                 item['credit_ranking'], item['job_description'], item['company_description']))
+        except Exception as e:
+            print(e)
 
-        self.book.save('ganji_xlwt.xls')
+        try:
+            with open('ganji2.csv', 'a+', newline='') as f2:
+                writer = csv.writer(f2)
+                writer.writerow((item['title'], item['position'], item['recruit'], item['location'], item['company'],
+                                 item['industry'], item['nature'], item['scale'], item['authentication'],
+                                 item['credit_ranking']))
+        except Exception as e:
+            pass
+
+        # self.sheet.write((item['title'], item['position'], item['recruit'], item['location'], item['company'],item['industry'], item['nature'],
+        #                   item['scale'], item['authentication'], item['credit_ranking'],item['job_description'], item['company_description']))
         #
+        # self.book.save('ganji_xlwt.xls')
+
         return item
