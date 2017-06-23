ebce6b90661bd32d5bc4ddcb24643a878197465b
Wangqingchen <541002901@qq.com>
2017-06-23 21:34:22 +0800

    update

--- ScrapyTrainingExercises/unit5/SeleniumSpiderWithParsel.py
+++ ScrapyTrainingExercises/unit5/SeleniumSpiderWithParsel.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 def scrape():
-    driver = webdriver.PhantomJS(executable_path=r'G:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+    # driver = webdriver.PhantomJS(executable_path=r'E:\files\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+    driver = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
     driver.get('https://www.amazon.cn/s/ref=nb_sb_noss_1/457-9456003-0676269?__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99&url=search-alias%3Dbooks&field-keywords=python&sprefix=python%2Caps%2CNaN&crid=2NW36ZVI3BPCN')
     while True:
         sel = parsel.Selector(text=driver.page_source)
--- ScrapyTrainingExercises/unit5/unit5/spiders/unit5.py
+++ ScrapyTrainingExercises/unit5/unit5/spiders/unit5.py
@@ -6,8 +6,8 @@ class Spider(scrapy.Spider):
     start_urls = ['https://www.amazon.cn/s/ref=nb_sb_ss_c_2_6?__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99&url=search-alias%3Dbooks&field-keywords=python&sprefix=python%2Caps%2C154&crid=71XLL8KCV31R']
 
     def __init__(self,*args,**kwargs):
-        # self.driver = webdriver.PhantomJS(executable_path=r'E:\files\phantomjs-2.1.1-windows\bin\phantomjs.exe')
-        self.driver = webdriver.PhantomJS(executable_path=r'G:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+        self.driver = webdriver.PhantomJS(executable_path=r'E:\files\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+        # self.driver = webdriver.PhantomJS(executable_path=r'G:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
         super(Spider, self).__init__(*args, **kwargs)
 
     def parse(self, response):
@@ -15,3 +15,8 @@ class Spider(scrapy.Spider):
         sel = scrapy.Selector(text=self.driver.page_source)
         for i in sel.xpath('//div[@class="a-row a-spacing-none"]/a/h2/text()').extract():
             yield {'book name': i}
+
+        next_page = sel.xpath('//span[@class="pagnRA"]/a[@class="pagnNext"]/@href').extract_first()
+        base_url = 'https://www.amazon.cn'
+        if next_page:
+            yield scrapy.Request(base_url + next_page, callback=self.parse)
