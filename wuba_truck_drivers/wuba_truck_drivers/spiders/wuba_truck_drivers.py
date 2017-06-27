13931be1f77b7841e66d8b9bf0b1c2f68bbe9fe4
Wangqingchen <541002901@qq.com>
2017-06-27 19:31:16 +0800

    update

--- wuba_truck_drivers/wuba_truck_drivers/spiders/wuba_truck_drivers.py
+++ wuba_truck_drivers/wuba_truck_drivers/spiders/wuba_truck_drivers.py
@@ -1,4 +1,4 @@
-import scrapy
+import scrapy,re
 from ..items import WubaTruckDriversItem
 
 class WubaTruckDrivers(scrapy.Spider):
@@ -25,18 +25,23 @@ class WubaTruckDrivers(scrapy.Spider):
 
     def driver_details(self, response):
         driver = WubaTruckDriversItem()
+        pattern = re.compile(r'[\u3000\xa0\u2003\xae\u2022\u200b\u200c\x81\u20e3\ufe0f\xad\u202a\u200d]')
         for item in response.xpath('//div[@class="item_con pos_info"]'):
-            driver['title'] = item.xpath('div[@class="pos_base_info"]/span[1]/text()').extract_first()
-            driver['position'] = item.xpath('span/text()').extract_first()
-            driver['recruit'] = item.xpath(
-                'div[@class="pos_base_condition"]/span[@class="item_condition pad_left_none"]/text()').extract_first()
-            driver['location'] = ''.join([str(i).strip() for i in item.xpath('div[@class="pos-area"]/span/a/text()').extract()])
-            driver['company'] = response.xpath(
-                '//div[@class="comp_baseInfo_title"]/div[@class="baseInfo_link"]/a/text()').extract_first()
-            # driver['scale']=item.xpath('')
-            driver['industry'] = response.xpath('//p[@class="comp_baseInfo_belong"]/a/text()').extract_first()
-            driver['scale'] = response.xpath('//p[@class="comp_baseInfo_scale"]/text()').extract_first()
-            driver['job_description'] = ''.join([str(i).strip() for i in response.xpath(
-                '//div[@class="item_con"]/div[1]/div[@class="posDes"]/div[@class="des"]/text()').extract()])
-            driver['company_description'] = ''.join([str(i).strip() for i in response.xpath('//div[@class="intro"]/div/p/text()').extract()])
+            title = item.xpath('div[@class="pos_base_info"]/span/text()').extract()
+            driver['title'] = ''.join([str(i) for i in title])
+            driver['position'] = ''.join([str(i) for i in item.xpath('span/text()').extract()])
+            recruit = item.xpath('div[@class="pos_base_condition"]/span[@class="item_condition pad_left_none"]/text()').extract()
+            driver['recruit'] = ''.join([str(i) for i in recruit])
+            location = item.xpath('div[@class="pos-area"]/span/descendant::text()').extract()
+            driver['location'] = ''.join([str(i).strip().replace('查看地图', '') for i in location])
+            company = response.xpath('//div[@class="comp_baseInfo_title"]/div[@class="baseInfo_link"]/a/text()').extract()
+            driver['company'] = ''.join([str(i) for i in company])
+            industry = response.xpath('//p[@class="comp_baseInfo_belong"]/a/text()').extract()
+            driver['industry'] = ''.join([str(i) for i in industry])
+            scale = response.xpath('//p[@class="comp_baseInfo_scale"]/text()').extract()
+            driver['scale'] = ''.join([str(i) for i in scale])
+            job_description = response.xpath('//div[@class="item_con"]/div[1]/div[@class="posDes"]/div[@class="des"]/text()').extract()
+            driver['job_description'] = pattern.sub('', ''.join([str(i).strip() for i in job_description]))
+            company_description = response.xpath('//div[@class="intro"]/div/p/text()').extract()
+            driver['company_description'] = pattern.sub('', ''.join([str(i).strip() for i in company_description]))
         yield driver
