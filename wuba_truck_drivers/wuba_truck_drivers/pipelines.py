004358298f17dadc00a267e5ec98fe636b7eb30a
Wangqingchen <541002901@qq.com>
2017-06-28 23:42:24 +0800

    update

--- wuba_truck_drivers/wuba_truck_drivers/pipelines.py
+++ wuba_truck_drivers/wuba_truck_drivers/pipelines.py
@@ -16,18 +16,13 @@ class WubaTruckDriversPipeline(object):
         self.cursor = self.connection.cursor()
 
     def process_item(self, item, spider):
-        sql = 'insert into wuba_truck_drivers (TITLE,POSITION,RECRUIT,LOCATION,COMPANY,' \
-              'INDUSTRY,SCALE,JOB_DESCRIPTION,COMPANY_DESCRIPTION) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-        self.cursor.execute(sql,(item['title'],item['position'],item['recruit'],item['location'],item['company'],
+        sql = "insert into wuba_truck_drivers2 (TITLE,POSITION,RECRUIT,LOCATION,COMPANY,INDUSTRY,SCALE," \
+              "JOB_DESCRIPTION,COMPANY_DESCRIPTION) values('{}','{}','{}','{}','{}','{}','{}','{}','{}')"
+        self.cursor.execute(sql.format(item['title'],item['position'],item['recruit'],item['location'],item['company'],
                             item['industry'],item['scale'],item['job_description'],item['company_description']))
         self.connection.commit()
 
-        with open('wuba_truck_drivers.csv', 'a+', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow((item['title'], item['position'], item['recruit'], item['location'], item['company'],
-                             item['industry'], item['scale']))
-
-        with open('wuba_truck_drivers2.csv', 'a+', newline='') as f2:
+        with open('wuba_truck_drivers.csv', 'a+', newline='') as f2:
             writer = csv.writer(f2)
             writer.writerow((item['title'], item['position'], item['recruit'], item['location'], item['company'],
                              item['industry'], item['scale'],item['job_description'],item['company_description']))
