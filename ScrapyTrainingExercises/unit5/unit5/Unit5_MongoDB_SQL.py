b1bce1e55132f073c953abf526c24be31fd5c51e
Falonie Wang <541002901@qq.com>
2017-08-17 20:55:57 +0800

    ScrapySpiders

--- ScrapyTrainingExercises/unit5/unit5/Unit5_MongoDB_SQL.py
+++ ScrapyTrainingExercises/unit5/unit5/Unit5_MongoDB_SQL.py
@@ -2,15 +2,19 @@ import pymongo
 
 client=pymongo.MongoClient(host='localhost',port=27017)
 db=client['employee']
+db1=client['employee1']
 #amazon=db['amazon_python_books']
 
 # for i,item in enumerate(db.amazon_python_books.find({}),1):
 #     print(i,item)
-#
+
 # # db['amazon_python_books'].rename('amazon_python_books_old')
 # print(db['amazon_python_books_old'].full_name)
 # print(db['amazon_python_books'].count())
 # # db['amazon_python_books_old'].rename('amazon_python_books_cn')
 # print(db['amazon_python_books'].name)
 # db['amazon_python_books'].copyTo('amazon_1')
-db['amazon_python_books'].remove()
+# db['amazon_python_books'].remove()
+db['amazon_python_books'].drop()
+# for i,m in enumerate(db1['lagou'].find({}),1):
+#     print(i,m)to
